b7642a44f3b88640ae4ce0135c7cfb6dc32227e6
jerome2710 <jeromeanker@145-24-224-236.wlan.hro.nl>
2017-02-20 13:35:03 +0100

    base app

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
-// http
-var http = require('http');
-var server = http.createServer();
-var port = parseInt(process.env.SERVER_PORT || '30043', 10) || 30043;
-var host = process.env.SERVER_HOST || 'localhost';
-server.listen(port, host, function () {
-    var host = server.address();
-    console.log('Listening on %s:%s', host.address, host.port);
+// Import the Express module
+var express = require('express');
+
+// Create a new instance of Express
+var app = express();
+
+// Serve static html, js, css, and image files from the 'public' directory
+app.use(express.static('public'));
+
+// Import the fs
+var fs = require('fs');
+
+// Import the Lanquiz game file.
+var lanquiz = require('./lanquiz');
+
+// Creating database file if not exists
+var file = 'playerstats.db';
+var exists = fs.existsSync(file);
+
+if (!exists) {
+    fs.openSync(file, 'w');
+}
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+    if (!exists) {
+        db.run('CREATE TABLE player (player_name TEXT, player_win INT)');
+    }
+});
+
+// Create a Node.js based http server on port 3000
+var server = require('http').createServer(app).listen(process.env.PORT || 3000);
+
+// Create a Socket.IO server and attach it to the http server
+var io = require('socket.io').listen(server);
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {
+    lanquiz.initGame(io, socket, db);
 });

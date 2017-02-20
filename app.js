eceffbcbf8592b8fd6a24c579b2f1dca026b48ea
jerome2710 <jeromeanker@145-24-224-236.wlan.hro.nl>
2017-02-20 14:22:03 +0100

    forever config

--- app.js
+++ app.js
@@ -40,3 +40,5 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
     lanquiz.initGame(io, socket, db);
 });
+
+console.log('Listening on port ' + (process.env.PORT || 3000));
